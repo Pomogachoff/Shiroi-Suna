e17360fb6e644faab8da18a00e48d4395e797c3b
Pomogach <pomogachofficial@gmail.com>
2025-04-25 23:00:26 +0500

    hello

--- js/js.js
+++ js/js.js
@@ -1,10 +1,29 @@
+const whiteScreen = document.querySelector(`.white-scr`);
+const loader = document.querySelector(`.loader`);
+let timerId;
+function loadIF(){
+    if(getComputedStyle(loader).opacity == 0){
+        loader.classList.add(`d-none`);
+        clearInterval(timerId);
+    }
+}
+function addSet(){
+    timerId = setInterval(loadIF, 100);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     anime({
-      targets: document.body,
-      opacity: 1,
-      duration: 1500,
-      easing: 'linear'
-    });
+      targets: whiteScreen,
+      opacity: 0,
+      duration: 3000,
+      easing: 'linear',
+      complete: ()=>{
+        whiteScreen.classList.add(`opacity-zero`);
+        whiteScreen.classList.add(`d-none`);
+        loader.classList.add(`opacity-zero`);
+        addSet();
+      }
+    }); 
 });
 
 const stars = document.querySelectorAll('.star');
@@ -14,23 +33,22 @@ const pRules = document.getElementById('p-rules');
 const firstPage = document.getElementById('firstPage');
 const secondPage = document.getElementById('secondPage');
 const thirdPage = document.getElementById('thirdPage');
-
+const leftRule = document.querySelector(`.left-strel`);
+const rightRule = document.querySelector(`.right-strel`);
 const toPage1 = document.getElementById(`page-1`);
 const toPage3 = document.getElementById(`page-3`);
 const toPage2Top = document.getElementById(`page-2-top`);
 const toPage2Bottom = document.getElementById(`page-2-bot`);
-
 const clientHeight = window.innerHeight * (-1);
-
 const rulesArr = [
     {'h4':'Спам', 'p':'одинаковые сообщения/гифки<br>если более 3 подряд сначала варн.<br>потом же бан и кик из флуда.'},
-    {'h4':'Оскорбления', 'p':'при оскорблении других участников в первый раз,<br>Вам выдаётся предупреждение, в случае повторных<br>казусов — мут на 3 часа. В случае если оскорбле-<br>ния перетекают в травлю, Вам выдаётся бан.'},
-    {'h4':'Контент 18+', 'p':'контент с материалами 18+ разрешён при условии,<br>что там имеется цензура и вы предупредили о своём наме-<br>рении. материалы с частично обнажёнными частями тела<br>(кроме пол. органов) не попадают под данное правило.'},
-    {'h4':'Ccоры', 'p':'желательно решать недопонимания в лс, но если же<br>такого не происходит и это просто перепалка, то так и быть.<br>ситуация, перерастающая в серьёзную ссору, которая портит<br>атмосферу чата, карается предупреждением, потом мутом мини-<br>мум на 3 часа. если ссора доходит до травли — бан навсегда.'},
+    {'h4':'Оскорбления', 'p':'при оскорблении других участников<br />в первый раз, Вам выдаётся преду-<br />преждение, в случае повторных ка-<br />зусов &mdash; мут на 3 часа. В случае если<br />оскорбления перетекают в травлю,<br />Вам выдаётся бан навсегда.'},
+    {'h4':'Контент 18+', 'p':'контент с материалами 18+ разрешён<br />при условии,что там имеется цензура<br />и вы предупредили о своём намерении.<br />материалы с частично обнажёнными ча-<br />стями тела(кроме половых органов) не<br /> попадают под действие данного правила.'},
+    {'h4':'Ccоры', 'p':'желательно решать недопонимания в лс,<br />но если же такого не происходит и это про-<br />сто перепалка, то так и быть. ситуация, пе-<br />рерастающая в серьёзную ссору, которая<br />портит атмосферу чата, карается предупре-<br />ждением, потом мутом минимум на 3 часа.<br />если дело доходит до травли &mdash; бан навсегда.'},
     {'h4':'Неактив', 'p':'неактив более 5 дней карается<br>баном. нью, которые в чате не более<br>5 дней, данное правило не касается.'},
-    {'h4':'Политика', 'p':'обсуждение политики строго-настрого<br>запрещено. за нарушение выдаётся предупреждение<br>при повторном нарушении — бан'},
-    {'h4':'Жалобы', 'p':'если на вас поступают 10 серьёзных жалоб, <br />которые были обоснованы, то вас кикают. мы <br />будем предупреждать о каждой жалобе, что<br />поступает и, конечно, проверять на досто-<br />верность и обоснованность тоже будем.'},
-    {'h4':'Незнание правил', 'p':'незнание правил не освобождает от ответственности'},
+    {'h4':'Политика', 'p':'обсуждение политики строго-настрого<br>запрещено. за нарушение выдаётся предупре-<br>ждениепри повторном нарушении — бан'},
+    {'h4':'Жалобы', 'p':'если на вас поступает 10 серьёзных жалоб,<br />которые были обоснованы, то вас кикают.<br />мы будем предупреждать о каждой жалобе,<br />и, конечно, проверять их на достоверность<br />и обоснованность тоже будем всегда.'},
+    {'h4':'Незнание правил', 'p':'не освобождает от ответственности'},
 ];
 
 function updateClasses(maxId) {
@@ -75,6 +93,30 @@ stars.forEach(star => {
         updateClasses(starId);
         updateRules(ruleIndex);
     });
+});
+leftRule.addEventListener('click', () => {
+    const starId = Number(document.querySelector(`.act`).id);
+    if(starId === 0){
+        starId = 14;
+    } else {
+        starId -= 2;
+    }
+    const ruleIndex = starId / 2;
+    
+    updateClasses(starId);
+    updateRules(ruleIndex);
+});
+rightRule.addEventListener('click', () => {
+    let starId = Number(document.querySelector(`.act`).id);
+    if(starId === 14){
+        starId = 0;
+    } else {
+        starId += 2;
+    }
+    const ruleIndex = starId / 2;
+    
+    updateClasses(starId);
+    updateRules(ruleIndex);
 });
 
 toPage2Top.addEventListener(`click`, ()=>{
